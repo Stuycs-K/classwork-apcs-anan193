43fc349f0b89571ca2826d306c8ed3039f19236e
Andy Jiang <ananjj193@gmail.com>
2024-10-01 20:11:15 -0400

    Added replaceNegative function and tested

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -65,6 +65,33 @@ public class ArrayMethods {
     return swap;
   }
 
+  //3. Modify a given 2D array of integer as follows:
+  //Replace all the negative values:
+  //-When the row number is the same as the column number replace
+  //that negative with the value 1
+  //-All other negatives replace with 0
+  public static void replaceNegative(int[][] vals){
+    for (int i = 0; i < vals.length; i++){
+      for (int k = 0; k < vals[i].length; k++){
+        if (i == k && vals[i][k] < 0){
+          vals[i][k] = 1;
+        }
+        if (vals[i][k] < 0){
+          vals[i][k] = 0;
+        }
+      }
+    }
+  }
+
+  //4. Make a copy of the given 2d array.
+  //When testing : make sure that changing the original does NOT change the copy.
+  //DO NOT use any built in methods that "copy" an array.
+  //You SHOULD write a helper method for this.
+  //If you don't see a good way to do that, you should stop and look at prior methods.
+  public static int[][] copy(int[][] nums){
+    return null;//placeholder so it compiles
+  }
+
   public static void main(String[] args) {
     int[][] test1 = new int[][] {{1}, {2}, {3}, {4}, {5}};
     int[][] test2 = new int[][] {{}, {}, {}};
@@ -73,6 +100,7 @@ public class ArrayMethods {
     int[][] test5 = new int[][] {{1,2,3}, {2,3}, {4,5,6,7,8}};
     int[][] test6 = new int[][] {{1,2}, {3,4}, {5,6}, {7,8}};
     int[][] test7 = new int[][] {{3,5,1}, {3,1,2}, {50,2,1}};
+    int[][] test8 = new int[][] {{-2,-2,1},{2,0,1,4},{-1,2,-4,4},{0,-1,-3,5,-4}};
     System.out.println("printing arrToString");
     System.out.println(arrToString(test1));
     System.out.println(arrToString(test2));
@@ -87,5 +115,9 @@ public class ArrayMethods {
     System.out.println(arrToString(swapRC(test1)));
     System.out.println(arrToString(swapRC(test6)));
     System.out.println(arrToString(swapRC(test7)));
+    System.out.println("printing replaceNegative");
+    System.out.println(arrToString(test8));
+    replaceNegative(test8);
+    System.out.println(arrToString(test8));
   }
 }
