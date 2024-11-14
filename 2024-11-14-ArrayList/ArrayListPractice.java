992602e7236ba27e5c54fcd12cbb1e411fb05c1d
Andy Jiang <andyj44@nycstudents.net>
2024-11-14 10:43:03 -0500

    wrote replaceEmpty

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -5,8 +5,23 @@ public class ArrayListPractice {
     ArrayList<String> a = new ArrayList<String>(size);
     for (int i = 0; i < size; i++) {
       int random = (int)(Math.random() * 10);
-      a.add("" + random);
+      if(random == 0){
+        a.add("");
+      }
+      else{
+        a.add("" + random);
+      }
     }
     return a;
   }
+  public static void replaceEmpty( ArrayList<String> original){
+  //Modify the ArrayList such that it has all of the empty strings are
+  //replaced with the word "Empty".\
+    for(int x = 0; x < original.length(); x++){
+      if(a.get(x).equals("")){
+        a.set(x, "Empty");
+      }
+    }
+  }
+
 }
