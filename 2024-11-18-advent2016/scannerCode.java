5bc0389f2b93858984db9ebc791e524f8bd8c2c1
Andy Jiang <andyj44@nycstudents.net>
2024-11-18 10:35:26 -0500

    Added today's directory

---
+++ 2024-11-18-advent2016/scannerCode.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CHANGE THIS {
+  public static int CHANGE THIS {
+    try{
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+
+
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return -1;
+    }
+  }
+}
