4185e837213db2bb463f8544bea87d4c63af6259
Andy Jiang <ananjj193@gmail.com>
2024-11-18 21:05:18 -0500

    finished part 1

--- 2024-11-18-advent2016/d1.java
+++ 2024-11-18-advent2016/d1.java
@@ -3,53 +3,50 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class d1 {
-  public static int d1(String filename) {
-    try{
-      File file = new File(filename);
-      Scanner input = new Scanner(file);
+    public static int d1(String filename) {
+        try {
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            int orientation = 0;
+            int x = 0;
+            int y = 0;
+            while (input.hasNext()) {
+                String direction = input.next();
+                if (direction.charAt(direction.length() - 1) == ',') {
+                    direction = direction.substring(0, direction.length() - 1);
+                }
+                char turn = direction.charAt(0);
+                int steps = Integer.parseInt(direction.substring(1));
+                
+                if (turn == 'R') {
+                    orientation = (orientation + 1) % 4;
+                } else if (turn == 'L') {
+                    orientation = (orientation - 1 + 4) % 4;
+                }
 
-      int orientation = 0;
-      int x = 0;
-      int y = 0;
+                if (orientation == 0) {
+                  y+=steps;
+                }
+                if (orientation == 1) {
+                  x+=steps;
+                }
+                if (orientation == 2) {
+                  y-=steps;
+                }
+                if (orientation == 3) {
+                  x-=steps;
+                }
+            }
 
-      while (input.hasNext()) {
-        String direction = input.next();
-        System.out.println(direction);
-        if (direction.charAt(0) == 'R') {
-          orientation = (orientation + 1);
+            return Math.abs(x) + Math.abs(y);
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+            return -1;
         }
-        if (direction.charAt(0) == 'L') {
-          orientation = (orientation - 1);
-        }
-        System.out.println(orientation);
-        if (direction.charAt(direction.length()-1) == ',') {
-          direction = direction.substring(1,direction.length()-1);
-        }
-        else {
-          direction = direction.substring(1,direction.length());
-        }
-        System.out.println(direction);
-        if (orientation%4 == 0) {
-          x += Integer.parseInt(direction);
-        }
-        if (orientation%4 == 1) {
-          y += Integer.parseInt(direction);
-        }
-        if (orientation%4 == 2) {
-          x -= Integer.parseInt(direction);
-        }
-        if (orientation%4 == 3) {
-          y -= Integer.parseInt(direction);
-        }
-      }
-      return x + y;
-    } catch (FileNotFoundException ex) {
-      System.out.println("File not found");
-      return -1;
     }
-  }
 
-  public static void main(String[] args) {
-    System.out.println(d1("d1_input.txt"));
-  }
+    public static void main(String[] args) {
+        System.out.println(d1("d1_input.txt"));
+    }
 }
