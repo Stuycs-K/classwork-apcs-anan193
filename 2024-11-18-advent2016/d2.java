08f541e0a5d9cf4de4ae2d6fe4a8ade418190381
Andy Jiang <andyj44@nycstudents.net>
2024-11-19 10:49:53 -0500

    did d2 part 1

---
+++ 2024-11-18-advent2016/d2.java
@@ -0,0 +1,67 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class d2 {
+  public static int d2a (String filename) {
+    try{
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+
+      int[][] numpad = {{1,2,3},{4,5,6},{7,8,9}};
+      int row = 1;
+      int col = 1;
+      while (input.hasNextLine()) {
+        String line = input.nextLine();
+        for (int i = 0; i < line.length(); i++) {
+          if (line.charAt(i) == 'R') {
+            if (col < 2) {
+              col++;
+            }
+          }
+          if (line.charAt(i) == 'L') {
+            if (col > 0) {
+              col--;
+            }
+          }
+          if (line.charAt(i) == 'U') {
+            if (row > 0) {
+              row--;
+            }
+          }
+          if (line.charAt(i) == 'D') {
+            if (row < 2) {
+              row++;
+            }
+          }
+        }
+        System.out.print(numpad[row][col]);
+      }
+      System.out.println("");
+      return 0;
+
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return -1;
+    }
+  }
+
+  public static int d2b (String filename) {
+    try{
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+
+      
+
+      return 1;
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return -1;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(d2a("d2_input.txt"));
+    System.out.println(d2b("d2_input.txt"));
+  }
+}
