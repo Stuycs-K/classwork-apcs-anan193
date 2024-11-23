665d87c0dc5f577f8bbaad95e1f903087b09ab10
Andy Jiang <ananjj193@gmail.com>
2024-11-22 21:14:48 -0500

    finished part 2

--- 2024-11-18-advent2016/d6.java
+++ 2024-11-18-advent2016/d6.java
@@ -37,6 +37,38 @@ public class d6 {
     return message;
   }
 
+  public static String minHash(ArrayList<String> a) {
+    String message = "";
+    for (int i = 0; i < a.get(0).length(); i++) { //choose a column
+      HashMap<String, Integer> chars = new HashMap<String, Integer>();
+      for (int j = 0; j < a.size(); j++) { //choose a row
+        String letter = a.get(j).substring(i,i+1);
+        if (chars.containsKey(letter)) {
+          int freq = chars.get(letter);
+          freq++;
+          chars.put(letter,freq);
+        }
+        else {
+          chars.put(letter,1);
+        }
+      }
+      int max_freq = 1000;
+      String min_letter = "";
+
+      for (String key : chars.keySet()) {
+        int frequency = chars.get(key);
+        if (frequency < max_freq) {
+          max_freq = frequency;
+          min_letter = key;
+        }
+      }
+      message += min_letter;
+    }
+    System.out.println(message);
+    return message;
+  }
+
+
   public static int d6a (String filename) {
     try{
       File file = new File(filename);
@@ -47,6 +79,7 @@ public class d6 {
         lines.add(a);
       }
       hash(lines);
+      minHash(lines);
       return 0;
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
