84442f4d5626c475859dc3be2ce93466df1fb3d5
Andy Jiang <andyj44@nycstudents.net>
2024-12-03 11:16:29 -0500

    saving

---
+++ 2024-12-03-colors/Color.java
@@ -0,0 +1,11 @@
+public class Color {
+  public static final String CLEAR_SCREEN =  "\u001b[2J";
+
+  public static void main(String[] args) {
+    System.out.print(CLEAR_SCREEN);
+    for (int x = 0; i <= 2 * 255; i++) {
+      for (int y = 0;)
+    }
+
+  }
+}
