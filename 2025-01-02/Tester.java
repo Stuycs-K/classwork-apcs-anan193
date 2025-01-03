9443e8bdfaacb0e4610e2fc2bdd6ba8672095304
Andy Jiang <andyj44@nycstudents.net>
2025-01-03 11:02:02 -0500

    finished makewords classwork

--- 2025-01-02/Tester.java
+++ 2025-01-02/Tester.java
@@ -1,10 +1,22 @@
 public class Tester {
   public static void main(String[] args) {
-    System.out.println(fib(46));
+    //System.out.println(fib(46));
+    makeWords(3,"","xy");
   }
   public static int fib(int n) {
     if (n == 0) return 0;
     else if (n == 1) return 1;
     else return fib(n-1)+fib(n-2);
+  }
+
+  public static void makeWords(int n, String partial, String alphabet) {
+    if (n == 0) {
+      System.out.println(partial);
+      return;
+    }
+    for (int i = 0; i < alphabet.length(); i++) {
+      char c = alphabet.charAt(i);
+      makeWords(n-1, partial + c, alphabet);
+    }
   }
 }
